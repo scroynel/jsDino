9f4bb5908769a0b0c4582c7d9d284c757a5e4cff
Vladislav Malyshko <scroynel@gmail.com>
2019-01-29 23:04:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,234 @@
+var body1 = document.querySelector('.body1'),
+    bodyColor1 = document.querySelector('.bodyColor1'),
+    body2 = document.querySelector('.body2'),
+    bodyColor2 = document.querySelector('.bodyColor2'),
+    body3 = document.querySelector('.body3'),
+    bodyColor3 = document.querySelector('.bodyColor3'),
+    body4 = document.querySelector('.body4'),
+    bodyColor4 = document.querySelector('.bodyColor4'),
+    body5 = document.querySelector('.body5'),
+    bodyColor5 = document.querySelector('.bodyColor5'),
+    body6 = document.querySelector('.body6'),
+    bodyColor6 = document.querySelector('.bodyColor6');
+
+bodyColor1.addEventListener('click', function(){
+    body1.style.display = 'block';
+    body2.style.display = 'none';
+    body3.style.display = 'none';
+    body4.style.display = 'none';
+    body5.style.display = 'none';
+    body6.style.display = 'none';
+})
+bodyColor2.addEventListener('click', function(){
+    body1.style.display = 'none';
+    body2.style.display = 'block';
+    body3.style.display = 'none';
+    body4.style.display = 'none';
+    body5.style.display = 'none';
+    body6.style.display = 'none';
+})
+bodyColor3.addEventListener('click', function(){
+    body1.style.display = 'none';
+    body2.style.display = 'none';
+    body3.style.display = 'block';
+    body4.style.display = 'none';
+    body5.style.display = 'none';
+    body6.style.display = 'none';
+})
+bodyColor4.addEventListener('click', function(){
+    body1.style.display = 'none';
+    body2.style.display = 'none';
+    body3.style.display = 'none';
+    body4.style.display = 'block';
+    body5.style.display = 'none';
+    body6.style.display = 'none';
+})
+bodyColor5.addEventListener('click', function(){
+    body1.style.display = 'none';
+    body2.style.display = 'none';
+    body3.style.display = 'none';
+    body4.style.display = 'none';
+    body5.style.display = 'block';
+    body6.style.display = 'none';
+})
+bodyColor6.addEventListener('click', function(){
+    body1.style.display = 'none';
+    body2.style.display = 'none';
+    body3.style.display = 'none';
+    body4.style.display = 'none';
+    body5.style.display = 'none';
+    body6.style.display = 'block';
+})
+
+var eye1 = document.querySelector('.eye1'),
+    eyeColor1 = document.querySelector('.eyeColor1'),
+    eye2 = document.querySelector('.eye2'),
+    eyeColor2 = document.querySelector('.eyeColor2'),
+    eye3 = document.querySelector('.eye3'),
+    eyeColor3 = document.querySelector('.eyeColor3'),
+    eye4 = document.querySelector('.eye4'),
+    eyeColor4 = document.querySelector('.eyeColor4');
+
+eyeColor1.addEventListener('click', function(){
+    eye1.style.display = 'block';
+    eye2.style.display = 'none';
+    eye3.style.display = 'none';
+    eye4.style.display = 'none';
+})
+eyeColor2.addEventListener('click', function(){
+    eye1.style.display = 'none';
+    eye2.style.display = 'block';
+    eye3.style.display = 'none';
+    eye4.style.display = 'none';
+})
+eyeColor3.addEventListener('click', function(){
+    eye1.style.display = 'none';
+    eye2.style.display = 'none';
+    eye3.style.display = 'block';
+    eye4.style.display = 'none';
+})
+eyeColor4.addEventListener('click', function(){
+    eye1.style.display = 'none';
+    eye2.style.display = 'none';
+    eye3.style.display = 'none';
+    eye4.style.display = 'block';
+})
+
+
+var head1 = document.querySelector('.head1'),
+    headColor1 = document.querySelector('.headColor1'),
+    head2 = document.querySelector('.head2'),
+    headColor2 = document.querySelector('.headColor2'),
+    head3 = document.querySelector('.head3'),
+    headColor3 = document.querySelector('.headColor3');
+    
+
+headColor1.addEventListener('click', function(){
+    head1.style.display = 'block';
+    head2.style.display = 'none';
+    head3.style.display = 'none';
+})
+headColor2.addEventListener('click', function(){
+    head1.style.display = 'none';
+    head2.style.display = 'block';
+    head3.style.display = 'none';
+})
+headColor3.addEventListener('click', function(){
+    head1.style.display = 'none';
+    head2.style.display = 'none';
+    head3.style.display = 'block';
+})
+
+var mouth1 = document.querySelector('.mouth1'),
+    mouthColor1 = document.querySelector('.mouthColor1'),
+    mouth2 = document.querySelector('.mouth2'),
+    mouthColor2 = document.querySelector('.mouthColor2'),
+    mouth3 = document.querySelector('.mouth3'),
+    mouthColor3 = document.querySelector('.mouthColor3');
+    
+
+mouthColor1.addEventListener('click', function(){
+    mouth1.style.display = 'block';
+    mouth2.style.display = 'none';
+    mouth3.style.display = 'none';
+})
+mouthColor2.addEventListener('click', function(){
+    mouth1.style.display = 'none';
+    mouth2.style.display = 'block';
+    mouth3.style.display = 'none';
+})
+mouthColor3.addEventListener('click', function(){
+    mouth1.style.display = 'none';
+    mouth2.style.display = 'none';
+    mouth3.style.display = 'block';
+})
+
+var scale1 = document.querySelector('.scale1'),
+    scaleColor1 = document.querySelector('.scaleColor1'),
+    scale2 = document.querySelector('.scale2'),
+    scaleColor2 = document.querySelector('.scaleColor2'),
+    scale3 = document.querySelector('.scale3'),
+    scaleColor3 = document.querySelector('.scaleColor3');
+    
+
+scaleColor1.addEventListener('click', function(){
+    scale1.style.display = 'block';
+    scale2.style.display = 'none';
+    scale3.style.display = 'none';
+})
+scaleColor2.addEventListener('click', function(){
+    scale1.style.display = 'none';
+    scale2.style.display = 'block';
+    scale3.style.display = 'none';
+})
+scaleColor3.addEventListener('click', function(){
+    scale1.style.display = 'none';
+    scale2.style.display = 'none';
+    scale3.style.display = 'block';
+})
+
+
+var spot1 = document.querySelector('.spot1'),
+    spotsColor1 = document.querySelector('.spotsColor1'),
+    spot2 = document.querySelector('.spot2'),
+    spotsColor2 = document.querySelector('.spotsColor2'),
+    spot3 = document.querySelector('.spot3'),
+    spotsColor3 = document.querySelector('.spotsColor3'),
+    spot4 = document.querySelector('.spot4'),
+    spotsColor4 = document.querySelector('.spotsColor4');
+
+spotsColor1.addEventListener('click', function(){
+    spot1.style.display = 'block';
+    spot2.style.display = 'none';
+    spot3.style.display = 'none';
+    spot4.style.display = 'none';
+})
+spotsColor2.addEventListener('click', function(){
+    spot1.style.display = 'none';
+    spot2.style.display = 'block';
+    spot3.style.display = 'none';
+    spot4.style.display = 'none';
+})
+spotsColor3.addEventListener('click', function(){
+    spot1.style.display = 'none';
+    spot2.style.display = 'none';
+    spot3.style.display = 'block';
+    spot4.style.display = 'none';
+})
+spotsColor4.addEventListener('click', function(){
+    spot1.style.display = 'none';
+    spot2.style.display = 'none';
+    spot3.style.display = 'none';
+    spot4.style.display = 'block';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
